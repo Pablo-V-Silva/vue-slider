c402b9d5d6b3546b82b4c7db7a33422b24176c0b
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-29 15:20:56 +0200

    add carousel and select image

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+const app = new Vue({
+  el: '#carousel',
+  data: {
+    images: [
+      {
+        imgSrc: '../img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+      {
+        imgSrc: '../img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+      {
+        imgSrc: '../img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+      {
+        imgSrc: '../img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+      {
+        imgSrc: '../img/05.jpg',
+        title: 'Paradise',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      },
+    ],
+    counter: 0,
+  },
+  methods: {
+
+  }
+})
